263a4925d26c32bf9e96df4994454d09bbafd5e4
alexander.klass <alexklass16@gmail.com>
2024-04-25 16:30:40 +0200

    Add Workflow for deploying GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     resolve: {},
+    base: process.env.NODE_ENV === 'production' ? '/bowling-site-fl/' : '/',
 });
